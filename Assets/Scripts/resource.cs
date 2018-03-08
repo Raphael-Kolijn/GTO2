f7feac9e11858d0122719f95f8f9ea184cb65616
Raphael Kolijn <raphaelkolijn@gmail.com>
2018-03-08 13:54:19 +0100

    Adding and subtracting to a resource
    Implemented a UI that allows the user to subtract or add a fixed amount
    to a chosen resource

---
+++ Assets/Scripts/resource.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resource : MonoBehaviour, IResource {
+
+    public int amount;
+    public string name;
+
+    public resource addResource()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<resource> resources()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void addAmount(int amountToAdd)
+    {
+        this.amount += amountToAdd;
+    }
+
+    public void subtractAmount(int amountToAdd)
+    {
+        this.amount -= amountToAdd;
+    }
+
+}
